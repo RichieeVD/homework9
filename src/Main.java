e6a2192b3f58452e498642a80a80310928e1f308
rvd <nseries_nokia@list.ru>
2026-02-24 18:45:27 +0400

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,14 @@ public class Main {
         } else if (clientOS == 1 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+
+        // Задача 3
+        int year = 2021;
+
+        if (year > 1584 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
