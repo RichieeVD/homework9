41c5a2736bfec6f23c7ca870ebff88e77845e428
rvd <nseries_nokia@list.ru>
2026-02-24 18:44:33 +0400

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,19 @@ public class Main {
         } else {
             System.out.println("Неизвестная операционная система");
         }
+
+        // Задача 2
+        clientOS = 0; // 0 — iOS, 1 — Android
+        int clientDeviceYear = 2015;
+
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientOS == 1 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
     }
 }
